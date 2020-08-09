fcb8bcd09a57f495ba1d6089354b758855e2128c
Jeferson <jeferson.santos@ilog.com.br>
2020-08-09 15:57:41 -0300

    Routes creater user and list by id

--- Pacote/server/src/routes.ts
+++ Pacote/server/src/routes.ts
@@ -1,22 +1,45 @@
 import express from "express";
+import db from "./database/connections";
 
 const routes  = express.Router();
 
-routes.post('/users', (request, response) =>{
-    console.log(request.body)
-    const users=[
-        {nome:"Jeferson",age:26},
-        {nome:"Jequelinege",age:24}
-    ]
-    return  response.json(users);
+routes.post('/classes', async(request, response) =>{
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    }=request.body;
+
+    const insertUserIds = await db('tb_users').insert({
+        name,
+        avatar,
+        whatsapp,
+        biografia:bio,
+    });
+
+    const userId = insertUserIds[0];
+
+    const insertClassIds= await db('tb_classes').insert({
+        subject,
+        cost,
+        userId,
+    });
+    const class_id = insertClassIds[0];
+
+    await db('tb_classes_schedule').insert({})
+    return response.send();
 })
 
-routes.get('/users', (request, response) =>{
-    const users=[
-        {nome:"Jeferson",age:26},
-        {nome:"Jequeline",age:24}
-    ]
-    return  response.json(users);
+routes.get('/users/:id', async (request, response) =>{
+    const {id} =request.params;
+    console.log(id)
+    const data2=await db('tb_users').select('name','whatsapp','biografia')
+        .where('id',id);
+    return  response.json(data2);
 })
 
 export default routes;
